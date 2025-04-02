16c856b13d548c42599e8975a1a50a0ca7b170b1
jeonghoon11 <jih361@naver.com>
2025-04-02 21:04:54 +0900

    [study] todo리스트를 만들며 리액트 복습

---
+++ Web/todo_review/src/components/Editor.jsx
@@ -0,0 +1,42 @@
+import { useState, useRef } from "react";
+
+import "./Editor.css";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
